fd12daf2f74a735d81f5e580897a71b4fd648c20
Iaroslav <GunGalla@gmail.com>
2022-12-05 11:40:26 +0200

    Add name cases.py in Chapter-2

---
+++ python_crash_course/Chapter-2/name_cases.py
@@ -0,0 +1,17 @@
+name = 'tirion lannister'
+
+print(f'Hello, {name}, how are you?')
+
+print(f'{name.lower()}\n{name.upper()}\n{name.title()}')
+
+famous_person = 'Nelson Mandela'
+
+print(f'{famous_person} once said:\n"The greatest glory in living lies not in never falling,'
+      ' but in rising every time we fall."')
+
+name = "\tKirill   "
+
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
