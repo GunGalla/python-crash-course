c0d795062b78681d7d37987758f25de70e963a6c
Iaroslav <GunGalla@gmail.com>
2022-12-05 12:53:36 +0200

    Add guests.py

---
+++ python_crash_course/Chapter-3/guests.py
@@ -0,0 +1,75 @@
+guests = ['Nikonor', 'Alexey', 'Svetlana', 'Tamara']
+
+print(f'I will be glad to invite you to have a dinner, {guests[0]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[1]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[2]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[3]}!')
+
+print(f"Unfortunately, {guests[1]}, won't be able to come :(")
+
+guests[1] = 'Evgeniia'
+
+print(f'I will be glad to invite you to have a dinner, {guests[0]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[1]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[2]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[3]}!')
+
+print("I've decided to invite additional people")
+
+guests.insert(0, 'Valentina')
+
+guests.insert(2, 'Kristina')
+
+guests.append('Pavel')
+
+print(f'I will be glad to invite you to have a dinner, {guests[0]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[1]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[2]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[3]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[4]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[5]}!')
+
+print(f'I will be glad to invite you to have a dinner, {guests[6]}!')
+
+print('Sorry for inconvenience, but I can invite only two people :(')
+
+feel_sorry = guests.pop()
+
+print(f'I am really sorry to decline my invitation, {feel_sorry}')
+
+feel_sorry = guests.pop()
+
+print(f'I am really sorry to decline my invitation, {feel_sorry}')
+
+feel_sorry = guests.pop()
+
+print(f'I am really sorry to decline my invitation, {feel_sorry}')
+
+feel_sorry = guests.pop()
+
+print(f'I am really sorry to decline my invitation, {feel_sorry}')
+
+feel_sorry = guests.pop()
+
+print(f'I am really sorry to decline my invitation, {feel_sorry}')
+
+print(f'Your invitation is still active, {guests[0]}!')
+
+print(f'Your invitation is still active, {guests[1]}!')
+
+del guests[0]
+
+del guests[0]
+
+print(guests)
