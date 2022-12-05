890310049da8c7bc9913150c9b641cf4a4b9ae6d
Iaroslav <GunGalla@gmail.com>
2022-12-05 17:42:44 +0200

    Add Chapter-5 and logic_operators.py

---
+++ python_crash_course/Chapter-5/logic_operators.py
@@ -0,0 +1,19 @@
+# Упражнения 5.1 и 5.2
+food = 'pizza'
+print("Is food == 'pizza'? I predict True.")
+print(food == 'pizza')
+
+print("\nIs food == 'burger'? I predict False.")
+print(food == 'burger')
+
+game = 'GOD OF WAR'
+print("\nIs game == 'Stray'? I predict False.")
+print(game == 'Stray')
+
+print("\nLet's check if game == 'GOD OF WAR'.")
+print(game == 'GOD OF WAR')
+
+hat_colour = 'green'
+
+print('\nGreen' == hat_colour)
+print('Green' == hat_colour.title())
