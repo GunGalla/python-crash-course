eac398fe84a2687d334d02c810d96491187cbbd2
Iaroslav <GunGalla@gmail.com>
2022-12-05 12:25:57 +0200

    Add Chapter-3 and some excercises

---
+++ python_crash_course/Chapter-3/cars.py
@@ -0,0 +1,3 @@
+car_models = ['Tesla', 'Toyota', 'Mercedes']
+
+print(f'I would like to buy a {car_models[0]} car!')
---
+++ python_crash_course/Chapter-3/friends_list.py
@@ -0,0 +1,13 @@
+friends = ['Alex', 'Kirill', 'Sasha']
+
+print(friends[0])
+
+print(friends[1])
+
+print(friends[2])
+
+print(f'Hello, {friends[0]}, can we met tomorrow?')
+
+print(f'Hello, {friends[1]}, can we met tomorrow?')
+
+print(f'Hello, {friends[2]}, can we met tomorrow?')
