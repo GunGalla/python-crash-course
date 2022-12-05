7f280097b98a7765d440dd9c7c33ede8c59e7236
Iaroslav <GunGalla@gmail.com>
2022-12-05 13:44:04 +0200

    Add Chapter-4 and animals.py, pizzas.py

---
+++ python_crash_course/Chapter-4/animals.py
@@ -0,0 +1,5 @@
+animals = ['dog', 'cat', 'parrot']
+
+for animal in animals:
+    print(f'I like {animal}s!')
+print('All these animals are great pets!')
---
+++ python_crash_course/Chapter-4/pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ['pepperoni', 'bbq', 'margarita']
+
+for pizza in pizzas:
+    print(f'I would like to order {pizza.title()} pizza!')
+print("I've ordered three pizzas!")
