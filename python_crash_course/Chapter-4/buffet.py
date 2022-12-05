5a945d0767fcf86ae00eba8769b5dd5c9354e5f2
Iaroslav <GunGalla@gmail.com>
2022-12-05 16:28:05 +0200

    Add buffet.py

---
+++ python_crash_course/Chapter-4/buffet.py
@@ -0,0 +1,11 @@
+# Упражнение 4.13
+
+buffet = ('fish', 'cheese', 'meat', 'pizza', 'cake')
+
+for food in buffet:
+    print(food.title())
+
+buffet = ('fish', 'cheese', 'coffee', 'burger', 'cake')
+
+for food in buffet:
+    print(food.title())
