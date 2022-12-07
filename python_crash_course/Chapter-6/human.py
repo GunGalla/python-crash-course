a8cdcfcf8618b03da810f2afad613adee984b47b
Iaroslav <GunGalla@gmail.com>
2022-12-07 14:17:59 +0200

    Add Chapter-6 dir and human.py

---
+++ python_crash_course/Chapter-6/human.py
@@ -0,0 +1,23 @@
+# Упражнения 6.1 - 6.3
+
+human = {
+    'first_name': 'Yaroslav',
+    'last_name': 'Kasatkin',
+    'age': 30,
+    'city': 'Saransk'
+    }
+
+print(human['first_name'])
+print(human['last_name'])
+print(human['age'])
+print(human['city'])
+
+favorite_nums = {
+    'Evgenia': 28,
+    'Yaroslav': 8,
+    'Vladimir': 2,
+    'Kirill': 10,
+    'Mob': 23,
+    }
+
+print(f"Evgenia's favorite number is:\n\t{favorite_nums['Evgenia']}!")
