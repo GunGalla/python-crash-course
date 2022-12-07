2221daff33065703bd4704162bbeed465190d457
Iaroslav <GunGalla@gmail.com>
2022-12-07 10:57:27 +0200

    Add colors.py, fruits.py and life_period.py

---
+++ python_crash_course/Chapter-5/fruits.py
@@ -0,0 +1,14 @@
+# Упражнение 5.7
+
+favorite_fruits = ['oranges', 'apples', 'pineapples']
+
+if 'bananas' in favorite_fruits:
+    print('You really like bananas!')
+if 'strawberry' in favorite_fruits:
+    print('You really like strawberry!')
+if 'oranges' in favorite_fruits:
+    print('You really like oranges!')
+if 'pineapples' in favorite_fruits:
+    print('You really like pineapples!')
+if 'berry' in favorite_fruits:
+    print('You really like berry!')
---
+++ python_crash_course/Chapter-5/life_periods.py
@@ -0,0 +1,16 @@
+# Упражнение 5.6
+
+age = 64
+
+if age < 2:
+    print('You are a baby!')
+elif 2 <= age < 4:
+    print('You are a tottler!')
+elif 4 <= age < 13:
+    print('You are a child!')
+elif 13 <= age < 20:
+    print('You are a teenager!')
+elif 20 <= age < 65:
+    print('You are an adult!')
+else:
+    print('You are a hi-ager!')
