e0ff6fc1e036bfac151b46c9b46403a09547a04d
Iaroslav <GunGalla@gmail.com>
2022-12-07 21:18:59 +0200

    Add Chapter-8 dr and func.py

---
+++ python_crash_course/Chapter-8/func.py
@@ -0,0 +1,13 @@
+# Упражнения 8.1 и 8.2
+def display_message():
+    message = 'Hello, now we should talk about functions.'
+    print(message)
+
+
+display_message()
+
+def favorite_book(title):
+    print(f'One of my favorite books is {title}!')
+
+
+favorite_book('Worm')
